32b406a0dd5e43b1379f5256c0689dd64f119b6a
Fayzul <fayzulhaq0708@gmail.com>
2024-07-03 01:38:04 +0800

    fix: change middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,79 +1,75 @@
-// import { NextResponse, NextRequest } from "next/server";
-// import { auth } from "../auth";
-
-// export async function middleware(request: NextRequest) {
-//   console.log("middleware");
-//   const session = await auth();
-
-//   if (!session) {
-//     return NextResponse.redirect(new URL("/signin", request.url));
-//   }
-
-//   switch (session?.user?.role) {
-//     case "CUSTOMER":
-//       if (
-//         !request.nextUrl.pathname.startsWith("/services") &&
-//         !request.nextUrl.pathname.startsWith("/feedback") &&
-//         !request.nextUrl.pathname.startsWith("/me")
-//       ) {
-//         console.log("Anda tidak punya akses ke halaman ini - customer");
-//         return NextResponse.redirect(new URL("/me", request.url));
-//       }
-//       break;
-//     case "ADMIN":
-//       if (!request.nextUrl.pathname.startsWith("/dashboard")) {
-//         console.log("Anda tidak punya akses ke halaman ini - ADMIN");
-//         return NextResponse.redirect(new URL("/dashboard", request.url));
-//       }
-//       break;
-//     default:
-//       return NextResponse.redirect(new URL("/signin", request.url));
-//   }
-// }
-
-// export const config = {
-//   matcher: ["/dashboard/:path*", "/me/:path*", "/services/:path*"],
-// };
-
 import { NextResponse, NextRequest } from "next/server";
 import { auth } from "../auth";
 
-export default auth((request) => {
-  console.log(request.auth?.user.role);
-  const user = request.auth?.user;
-  const role = user.role;
+const unauthenticatedRoutes = ["/signin", "/signup"];
 
+export default auth((request) => {
   console.log("middleware");
-  // const user = await auth();
+  const user = request.auth?.user;
 
   if (!user) {
+    console.log("Anda harus login dulu");
+    if (unauthenticatedRoutes.includes(request.nextUrl.pathname)) {
+      return NextResponse.next();
+    }
     return NextResponse.redirect(new URL("/signin", request.nextUrl.origin));
+  } else {
+    console.log("Anda sudah login");
+    if (request.nextUrl.pathname === "/api/auth/signout") {
+      return NextResponse.next();
+    }
   }
+
+  const role = user?.role;
 
   switch (role) {
     case "CUSTOMER":
       if (
         !request.nextUrl.pathname.startsWith("/services") &&
-        !request.nextUrl.pathname.startsWith("/feedback") &&
         !request.nextUrl.pathname.startsWith("/me")
       ) {
         console.log("Anda tidak punya akses ke halaman ini - customer");
+
+        if (unauthenticatedRoutes.includes(request.nextUrl.pathname)) {
+          return NextResponse.redirect(new URL("/me", request.nextUrl.origin));
+        } else {
+          console.log("unauthenticatedRoutes else - customer");
+        }
         return NextResponse.redirect(new URL("/me", request.nextUrl.origin));
+      } else {
+        console.log("Customer else");
       }
       break;
     case "ADMIN":
       if (!request.nextUrl.pathname.startsWith("/dashboard")) {
         console.log("Anda tidak punya akses ke halaman ini - ADMIN");
+        if (unauthenticatedRoutes.includes(request.nextUrl.pathname)) {
+          return NextResponse.redirect(
+            new URL("/dashboard", request.nextUrl.origin),
+          );
+        } else {
+          console.log("unauthenticatedRoutes else - ADMIN");
+        }
+
         return NextResponse.redirect(
           new URL("/dashboard", request.nextUrl.origin),
         );
+      } else {
+        console.log("Admin else");
       }
       break;
     default:
-      return NextResponse.redirect(new URL("/signin", request.nextUrl.origin));
+      return NextResponse.redirect(new URL("/", request.nextUrl.origin));
   }
 });
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/me/:path*", "/services/:path*"],
+  matcher: [
+    "/dashboard/:path*",
+    "/me/:path*",
+    "/services/:path*",
+    "/signin",
+    "/signup",
+    "/api/auth/signout",
+  ],
 };
